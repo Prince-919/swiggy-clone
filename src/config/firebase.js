17f7c59771856e05b0e44601d60cb67619f05678
Prince-919 <princesharmabbu0@gmail.com>
2025-06-30 17:41:01 +0530

    add firebase configuare

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,7 +1,14 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
-const firebaseConfig = JSON.parse(import.meta.env.VITE_KEY);
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDERID,
+  appId: import.meta.env.VITE_APPID,
+};
 
 export const app = initializeApp(firebaseConfig);
 
